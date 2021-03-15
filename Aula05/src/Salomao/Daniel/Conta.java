e86d32803eb28172200070a03361f3359cc26d12
DanielCSalomao <DanielCSalomao@users.noreply.github.com>
2021-03-15 13:00:21 -0300

    Programa aula05

--- Aula05/src/Salomao/Daniel/Conta.java
+++ Aula05/src/Salomao/Daniel/Conta.java
@@ -1,17 +1,26 @@
 package Salomao.Daniel;
 
 public class Conta {
-    int numero;
-    double saldo;
-    Cliente cliente;
-    void visualizarSalto() {
-        System.out.println("Saldo da conta: R$" + this.saldo);
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+
+    public Conta(int numero, double saldo, String cliente) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cliente = new Cliente(cliente);
     }
-    void depositar(double valor) {
+
+    public double getSaldo() {
+
+        return this.saldo;
+    }
+
+    public void depositar(double valor) {
         //this.saldo = this.saldo + valor;
         this.saldo += valor;
     }
-    boolean sacar(double valor) {
+    public boolean sacar(double valor) {
         if (this.saldo >= valor){
             this.saldo -= valor;
             return true;
--- Aula05/src/Salomao/Daniel/Sistema.java
+++ Aula05/src/Salomao/Daniel/Sistema.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Sistema {
     private boolean continuarExecucao;
     private Scanner scanner;
+    private Conta conta;
 
     public Sistema() {
         this.continuarExecucao = true;
         this.scanner = new Scanner(System.in);
+        this.conta = new Conta(1234, 1000, "Toad");
     }
 
     public void executar(){
@@ -25,6 +27,39 @@ public class Sistema {
                 System.out.println("Obrigado por ter utilizado nosso sistema!");
                 this.continuarExecucao = false;
                 break;
+            case 1:
+                System.out.println("Saldo: R$" + this.conta.getSaldo());
+                break;
+            case 2:
+                System.out.println("Informe o valor: ");
+                double valorParaDepositar = scanner.nextDouble();
+                this.conta.depositar(valorParaDepositar);
+                System.out.println("Operação realizada com sucesso!");
+                break;
+            case 3:
+                System.out.println("Informe o valor: ");
+                double valorParaSaque = scanner.nextDouble();
+                if(this.conta.sacar(valorParaSaque)) {
+                    System.out.println("Operação realizada com sucesso!");
+                } else {
+                    System.out.println("Operação falhou! Verificar Saldo!");
+                }
+                break;
+            case 5:
+                System.out.println("Nome do titulo:");
+                String nomeTitulo= scanner.next();
+                System.out.println("Informe o valor do titulo:");
+                double valorTitulo = scanner.nextDouble();
+                System.out.println("Informe o valor do juros ao dia:");
+                double valorJurosTitulo = scanner.nextDouble();
+                System.out.println("Infome a data de vencimento aaaa-mm-dd:");
+                String dataValidade = scanner.next();
+                Titulo titulo = new Titulo(valorTitulo,valorJurosTitulo,nomeTitulo,dataValidade);
+                if(this.conta.sacar(titulo.getTotalPagamento())){
+                    System.out.println("Operacao realizada com sucesso");
+                }else{
+                    System.out.println("Falha na operacao");
+                }
             default:
                 System.out.println("Funcionalidade ainda não implementada");
                 break;
