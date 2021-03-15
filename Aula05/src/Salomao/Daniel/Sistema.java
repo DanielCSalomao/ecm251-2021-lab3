63b01e1e5c39df5caaefd6fcb32e316b629c0b8e
DanielCSalomao <DanielCSalomao@users.noreply.github.com>
2021-03-15 12:13:40 -0300

    Criar loop de execução do sistema

--- Aula05/src/Salomao/Daniel/Sistema.java
+++ Aula05/src/Salomao/Daniel/Sistema.java
@@ -1,7 +1,44 @@
 package Salomao.Daniel;
 
+import java.util.Scanner;
+
 public class Sistema {
+    private boolean continuarExecucao;
+    private Scanner scanner;
+
+    public Sistema() {
+        this.continuarExecucao = true;
+        this.scanner = new Scanner(System.in);
+    }
+
     public void executar(){
-        System.out.println("Aqui!");
+        while(continuarExecucao) {
+            exibirMenu();
+            int opcao = scanner.nextInt();
+            avaliarOpcao(opcao);
+        }
+    }
+
+    private void avaliarOpcao(int opcao) {
+        switch (opcao){
+            case 0:
+                System.out.println("Obrigado por ter utilizado nosso sistema!");
+                this.continuarExecucao = false;
+                break;
+            default:
+                System.out.println("Funcionalidade ainda não implementada");
+                break;
+        }
+    }
+
+    private void exibirMenu() {
+        System.out.println("Bem vindo ao MauaBank");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar dinheiro");
+        System.out.println("3 - Sacar dinheiro");
+        System.out.println("4 - Transferir dinheiro");
+        System.out.println("5 - Pagar título");
+        System.out.println("0 - Sair do sistema");
+
     }
 }
