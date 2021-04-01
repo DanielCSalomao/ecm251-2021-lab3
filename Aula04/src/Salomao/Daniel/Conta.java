b917bd68691d4f022322b5df766ebed73578a6f9
DanielCSalomao <DanielCSalomao@users.noreply.github.com>
2021-04-01 16:26:20 -0300

    Aula04

--- Aula04/src/Salomao/Daniel/Conta.java
+++ Aula04/src/Salomao/Daniel/Conta.java
@@ -1,46 +1,67 @@
 package Salomao.Daniel;
 
 public class Conta {
-    private int numero;
-    private double saldo;
-    private Cliente cliente;
-
-    //metodos de classe
-    public void setSaldo(double novoSaldo){
-        this.saldo - saldo;
+    public Cliente getCliente() {
+        return cliente;
     }
-    public void getSaldo(){
+
+    public int getNumero() {
+        return numero;
+    }
+
+    //Atributos das classe
+    private Cliente cliente;
+    private double saldo;
+    private int numero;
+
+    //Métodos da classe
+
+    //Construtor da classe
+    public Conta(String nomeCliente, double saldoInicial){
+        this.saldo = saldoInicial;
+        this.cliente = new Cliente(nomeCliente);
+        this.numero = 1234;
+    }
+    public Conta(String nomeCliente, String cpf){
+        this.cliente = new Cliente(nomeCliente);
+    }
+
+    public double getSaldo(){
         return this.saldo;
     }
 
-    public void visualizarSalto() {
-        System.out.println("Saldo da conta: R$" + this.saldo);
-    }
     public void depositar(double valor) {
         //this.saldo = this.saldo + valor;
         this.saldo += valor;
     }
+
     public boolean sacar(double valor) {
-        if (this.saldo >= valor){
+        if(valor <= this.saldo){
             this.saldo -= valor;
             return true;
         }
         return false;
     }
-    public boolean transferirPara(double valor, Conta destino) {
-        if (this.sacar(valor)){
+
+    public boolean transferirDinheiro(Conta destino, double valor) {
+        if(this.sacar(valor)){
             destino.depositar(valor);
             return true;
         }
         return false;
     }
+
+
+    public void visualizarSaldo() {
+        System.out.println("Saldo atual: R$" + this.saldo);
+    }
+
     @Override
     public String toString() {
         return "Conta{" +
-                "numero=" + numero +
+                "cliente=" + cliente.toString() +
                 ", saldo=" + saldo +
-                ", cliente=" + cliente.toString() +
+                ", numero=" + numero +
                 '}';
     }
 }
-
