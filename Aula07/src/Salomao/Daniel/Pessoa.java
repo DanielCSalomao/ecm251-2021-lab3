239873ce7f289b5de6decdb5052eb7470ba14ccb
DanielCSalomao <DanielCSalomao@users.noreply.github.com>
2021-04-19 11:50:30 -0300

    Aula07

---
+++ Aula07/src/Salomao/Daniel/Pessoa.java
@@ -0,0 +1,30 @@
+package Salomao.Daniel;
+
+public class Pessoa {
+    private final String nome;
+    private final String cpf;
+    public int mudar;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String Ola(){
+        return "Ola! Eu sou " + this.nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", mudar=" + mudar  +
+                '}';
+    }
+}
+© 2021 GitHub, Inc.
+        Terms
+        Privacy
+        Security
+        Status
