9755a6aa1694164d867a41e79592d1d5c8939483
DanielCSalomao <DanielCSalomao@users.noreply.github.com>
2021-06-16 16:51:13 -0300

    Update BigBrothers.java

--- AtividadeT2/src/Salomao/Daniel/models/membros/BigBrothers.java
+++ AtividadeT2/src/Salomao/Daniel/models/membros/BigBrothers.java
@@ -3,7 +3,10 @@ package Salomao.Daniel.models.membros;
 import Salomao.Daniel.models.Membro;
 
 public class BigBrothers extends Membro {
+
     public BigBrothers(String nome, String funcao, String email){
         super(nome, funcao, email);
     }
+
+
 }
